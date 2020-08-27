09cd8830079093519b8f0556e98f9d674789d5fa
Ahmersaeed09 <51811391+Ahmersaeed09@users.noreply.github.com>
2020-08-27 17:13:59 +0500

    40% work of mobile client
    40% implementation

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Icon from "react-native-vector-icons/Ionicons";
+import MainTabScreen from "./Screens/MainTabScreen";
+import { DrawerContent } from "./Screens/DrawerContent";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import SplashScreen from "./Screens/SplashScreen";
+import SignInScreen from "./Screens/SignInScreen";
+import SignUpScreen from "./Screens/SignUpScreen";
+const RootStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+import RootStackScreen from "./Screens/RootStackScreen";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/* <RootStack.Navigator headerMode="none">
+        <RootStack.Screen name="SplashScreen" component={SplashScreen} />
+        <RootStack.Screen name="SignInScreen" component={SignInScreen} />
+      </RootStack.Navigator> */}
+
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
+    <Drawer.Screen name="Home" component={MainTabScreen} />
+  </Drawer.Navigator>
+</NavigationContainer>
+  );
+}
